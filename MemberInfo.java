f9ed36e902ec2b76738ede4b250162cec20796d9
1phillipt <78433509+1phillipt@users.noreply.github.com>
2021-12-20 17:38:18 -0500

    files added!

---
+++ MemberInfo.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class MemberInfo {
+    List<newMember> members = new ArrayList<>();
+
+  public void addMembers(newMember member){
+      members.add(member);
+  }
+
+    }
+
+
