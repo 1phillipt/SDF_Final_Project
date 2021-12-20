ef1283fba61bb7b025147752faed518fc88336b5
1phillipt <78433509+1phillipt@users.noreply.github.com>
2021-12-20 18:11:53 -0500

    reviewed!!

---
+++ Registration.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Registration {
+    List<newMember> members = new ArrayList<>();
+
+  public void addMembers(newMember member){
+      members.add(member);
+  }
+
+    }
+
+
