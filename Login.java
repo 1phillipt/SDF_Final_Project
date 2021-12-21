62f86497281be91588e0d7195b28401a1baac00c
1phillipt <78433509+1phillipt@users.noreply.github.com>
2021-12-20 23:45:51 -0500

    final update!!

--- Login.java
+++ Login.java
@@ -1,9 +1,10 @@
+
 import java.util.Scanner;
 
 public class Login extends Registration {
 
     private static Registration members;
-   static boolean registered = false;
+    static boolean registered = false;
     public static void main(String[] args) {
 
         String choice;
@@ -30,80 +31,92 @@ public class Login extends Registration {
             newUser =  userInput.next();
         }
 
-     while(mainChoice) {
-         if (newUser.equalsIgnoreCase("y")) {
-             registered = true;
-             System.out.println("Please enter your first name");
-             while (!isFirstLastNameValid(fName = userInput.next())) {
-                 System.out.println("Please put valid first name");
-             }
-             System.out.println("Please enter your Last name");
-             while (!isFirstLastNameValid(lName = userInput.next())) {
-                 System.out.println("Please put valid last name");
-             }
-                
-             System.out.println("Please enter your email!");//also verifies if @ and .com
-             while (!isEmailValid(email = userInput.next())) {
-                 System.out.println("Please put valid email");
-             }
-             System.out.println("Please enter your user name!");
-             while (!isUserNameValid(userName = userInput.next())) {
-                 System.out.println("Please put valid userName ");
-             }
-             System.out.println("Please enter your desired password at least 5 characters long and with atleast a number!");
-             while (!isPasswordValid(password = userInput.next())) {
-                 System.out.println("Please put valid password");
-             }
-             newMember member = new newMember(fName, lName, email, userName, password);
+        while(mainChoice) {
+            if (newUser.equalsIgnoreCase("y")) {
+                registered = true;
+                System.out.println("Please enter your first name");
+                while (!isFirstLastNameValid(fName = userInput.next())) {
+                    System.out.println("Please put valid first name");
+                }
+                System.out.println("Please enter your Last name");
+                while (!isFirstLastNameValid(lName = userInput.next())) {
+                    System.out.println("Please put valid last name");
+                }
+
+                System.out.println("Please enter your email!");//also verifies if @ and .com
+                while (!isEmailValid(email = userInput.next())) {
+                    System.out.println("Please put valid email");
+                }
+                System.out.println("Please enter your user name!");
+                while (!isUserNameValid(userName = userInput.next())) {
+                    System.out.println("Please put valid userName ");
+                }
+                System.out.println("Please enter your desired password at least 5 characters long and with atleast a number!");
+                while (!isPasswordValid(password = userInput.next())) {
+                    System.out.println("Please put valid password");
+                }
+                newMember member = new newMember(fName, lName, email, userName, password);
 
 
-             members.addMembers(member);
+                members.addMembers(member);
 
 
-             System.out.println("successfully registered!! Congrats");
+                System.out.println("successfully registered!! Congrats");
 
-             System.out.println("Do you want to login(y/n): ");
-             choice = userInput.next();
-             if (choice.equalsIgnoreCase("y")) {
-                 System.out.println("please provide your login id");
-                 String id = userInput.next();
-                 System.out.println("please provide the password now:");
-                 password = userInput.next();
-                 isMember(id, password);
-             }
+                System.out.println("Do you want to login(y/n): ");
+                choice = userInput.next();
+                if (choice.equalsIgnoreCase("y")) {
+                    System.out.println("please provide your login id");
+                    String id = userInput.next();
+                    System.out.println("please provide the password now:");
+                    password = userInput.next();
+                    isMember(id, password);
+                }
 
-         } else if (newUser.equalsIgnoreCase("n"))
-         {
-             System.out.println("Id-abc456 Password - 123456");
-             System.out.println("please provide your login id");
-             String id;
-              id = userInput.next();
-             System.out.println("please provide the password now:");
-             password = userInput.next();
-             isMember(id, password);
+            } else if (newUser.equalsIgnoreCase("n"))
+            {
+              if(registered) {
+                  System.out.println("please provide your login id");
+                  String id;
+                  id = userInput.next();
+                  System.out.println("please provide the password now:");
+                  password = userInput.next();
+                  isMember(id, password);
+              }  else{
+                  //even if user says "no" to new user!! program does not recognize them so here are info to login
+                  System.out.println("-----------------------------");
+                  System.out.println("login Id: phillip   Password: thapa01");
+                  System.out.println("These are your login id and password use them below!!!");
+                  System.out.println("-----------------------------");
+                  System.out.println("please provide your login id");
+                  String id;
+                  id = userInput.next();
+                  System.out.println("please provide the password now:");
+                  password = userInput.next();
+                  isMember(id, password);
+              }
+            }
+            System.out.println("want to continue the program(y/n): ");
 
-         }
-         System.out.println("want to continue the program(y/n): ");
+            String continueProgram = userInput.nextLine();
+            while (!continueProgram.equalsIgnoreCase("y") && !continueProgram.equalsIgnoreCase("n")) {
+                System.out.println("Please put Y or N");
+                continueProgram =  userInput.next();
 
-         String continueProgram = userInput.nextLine();
-         while (!continueProgram.equalsIgnoreCase("y") && !continueProgram.equalsIgnoreCase("n")) {
-             System.out.println("Please put Y or N");
-             continueProgram =  userInput.next();
+            }
+            if(continueProgram.equalsIgnoreCase("n")){
+                mainChoice = false;
+                System.out.println("Good bye!!");
+            }else {
+                System.out.println("are you a new user (y/n)");
+                newUser =  userInput.next();
 
-         }
-         if(continueProgram.equalsIgnoreCase("n")){
-             mainChoice = false;
-             System.out.println("Good bye!!");
-         }else {
-             System.out.println("are you a new user (y/n)");
-              newUser =  userInput.next();
-
-             while (!newUser.equalsIgnoreCase("y") && !newUser.equalsIgnoreCase("n")) {
-                 System.out.println("Please put Y or N");
-                 newUser =  userInput.next();
-             }
-         }
-     }
+                while (!newUser.equalsIgnoreCase("y") && !newUser.equalsIgnoreCase("n")) {
+                    System.out.println("Please put Y or N");
+                    newUser =  userInput.next();
+                }
+            }
+        }
 
     }
 
@@ -127,16 +140,16 @@ public class Login extends Registration {
                 return false;
             }
         }
-         if(loginGood) {
-             for (int i =0; i < members.members.size(); i++){
-                 if (members.members.get(i).password.equalsIgnoreCase(userPassword)) {
-                     System.out.println("Login Successful!!");
-                     return true;
-                 }
-             }
-         }else {
-             System.out.println("invalid password.. Try again");
-         }
+        if(loginGood) {
+            for (int i =0; i < members.members.size(); i++){
+                if (members.members.get(i).password.equalsIgnoreCase(userPassword)) {
+                    System.out.println("Login Successful!!");
+                    return true;
+                }
+            }
+        }else {
+            System.out.println("invalid password.. Try again");
+        }
 
         return false;
     }
@@ -149,37 +162,37 @@ public class Login extends Registration {
                 return false;
             }
         }
-            return true;
+        return true;
 
     }
 
-        public static boolean isEmailValid (String email) {
-            boolean containsHashTag = false;
-            if (email.length() < 6) {
-                return false;
-            } else {
-                for (int i = 1; i < email.length(); i++) {
-                    if (email.substring(i, i + 1).equalsIgnoreCase("@")) {
-                        containsHashTag = true;
-                    }
+    public static boolean isEmailValid (String email) {
+        boolean containsHashTag = false;
+        if (email.length() < 6) {
+            return false;
+        } else {
+            for (int i = 1; i < email.length(); i++) {
+                if (email.substring(i, i + 1).equalsIgnoreCase("@")) {
+                    containsHashTag = true;
                 }
-                if (containsHashTag) {
-                    for (int i = email.indexOf("@"); i < email.length(); i++) {
-                        if (Character.isDigit(email.charAt(i)) && !email.substring(email.length() - 4, email.length()).equalsIgnoreCase(".com")
-                                && !email.substring(email.length() - 4, email.length()).equalsIgnoreCase(".org")
-                                && !email.substring(email.length() - 4, email.length()).equalsIgnoreCase(".net"))
-                        {
-                            return false;
-                        }
-                    }
-                } else {
-                    //returns false if email does not contain @
-                    return false;
-                }
-                return true;
             }
+            if (containsHashTag) {
+                for (int i = email.indexOf("@"); i < email.length(); i++) {
+                    if (Character.isDigit(email.charAt(i)) && !email.substring(email.length() - 4, email.length()).equalsIgnoreCase(".com")
+                            && !email.substring(email.length() - 4, email.length()).equalsIgnoreCase(".org")
+                            && !email.substring(email.length() - 4, email.length()).equalsIgnoreCase(".net"))
+                    {
+                        return false;
+                    }
+                }
+            } else {
+                //returns false if email does not contain @
+                return false;
+            }
+            return true;
         }
-        public static boolean  isUserNameValid (String userName) {
+    }
+    public static boolean  isUserNameValid (String userName) {
         if (userName.length() < 5) {
             System.out.println("Please make sure user Id is at least 5 characters long");
             return false;
@@ -187,21 +200,21 @@ public class Login extends Registration {
             return true;
         }
     }
-        public static boolean  isPasswordValid (String password) {
-            boolean containsInt = false;
-            if (password.length() >= 5) {
-                for (int i = 0; i < password.length(); i++) {
-                    if (Character.isDigit(password.charAt(i))) {
-                        containsInt = true;
-                    }
+    public static boolean  isPasswordValid (String password) {
+        boolean containsInt = false;
+        if (password.length() >= 5) {
+            for (int i = 0; i < password.length(); i++) {
+                if (Character.isDigit(password.charAt(i))) {
+                    containsInt = true;
                 }
-                if (!containsInt) {
-                    return false;
-                }
-            } else {
-                System.out.println("Please make at least 5 characters long password with at least one number!");
+            }
+            if (!containsInt) {
                 return false;
             }
+        } else {
+            System.out.println("Please make at least 5 characters long password with at least one number!");
+            return false;
+        }
         return true;
     }
 
